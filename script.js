94a408a85c922fee3a5b2357a7694bf2a19317cc
WildMountainAir <alison.sipos@icloud.com>
2021-04-05 09:39:38 -0600

    Added cart checkout functionality.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const alisTagFunction = () => {
 }
 
 $(document).ready(function() {
-
+  // Product Page
   $('.color-choose input').on('click', function() {
       var headphonesColor = $(this).attr('data-image');
 
@@ -11,5 +11,23 @@ $(document).ready(function() {
       $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
       $(this).addClass('active');
   });
-
+  // Shopping Cart
+  $('.method').on('click', function() {
+    $('.method').removeClass('blue-border');
+    $(this).addClass('blue-border');
+  });
+ 
+})
+var $cardInput = $('.input-fields input');
+ 
+$('.next-btn').on('click', function(e) {
+ 
+  $cardInput.removeClass('warning');
+ 
+  $cardInput.each(function() {    
+     var $this = $(this);
+     if (!$this.val()) {
+       $this.addClass('warning');
+     }
+  })
 });
