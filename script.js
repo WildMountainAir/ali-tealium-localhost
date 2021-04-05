93b8ad335b1f1cc4f16814aba8219550997929ec
WildMountainAir <alison.sipos@icloud.com>
2021-04-05 15:57:31 -0600

    Updated document load script

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const alisTagFunction = () => {
 }
 
 $(document).ready(function() {
+  
   // Product Page
   $('.color-choose input').on('click', function() {
       var headphonesColor = $(this).attr('data-image');
@@ -17,17 +18,17 @@ $(document).ready(function() {
     $(this).addClass('blue-border');
   });
  
+  var $cardInput = $('.input-fields input');
+   
+  $('.next-btn').on('click', function(e) {
+   
+    $cardInput.removeClass('warning');
+   
+    $cardInput.each(function() {    
+       var $this = $(this);
+       if (!$this.val()) {
+         $this.addClass('warning');
+       }
+    })
+  });
 })
-var $cardInput = $('.input-fields input');
- 
-$('.next-btn').on('click', function(e) {
- 
-  $cardInput.removeClass('warning');
- 
-  $cardInput.each(function() {    
-     var $this = $(this);
-     if (!$this.val()) {
-       $this.addClass('warning');
-     }
-  })
-});
